9fe02179c52b670a4bb11ff3597e6f5be7480bb6
jyung17 <iotaxixi@gmail.com>
2022-07-28 20:48:25 +0900

    0728

---
+++ java-lang/app/src/main/java/com/eomcs/exception/ex1/test/Exam0210.java
@@ -0,0 +1,12 @@
+package com.eomcs.exception.ex1.test;
+
+import java.util.Scanner;
+
+public class Exam0210 {
+  public static void main(String[] args) {
+    Scanner keySacn = new Scanner(System.in);
+    while (ture) {
+
+    }
+  }
+}
