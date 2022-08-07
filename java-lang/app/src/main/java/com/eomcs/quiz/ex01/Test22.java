fad5402ad430d9ee9be958c9a8a1a65da761d547
jyung17 <iotaxixi@gmail.com>
2022-08-08 07:49:59 +0900

    ex01.Test21,22/ex02.Test09 quiz

--- java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test22.java
+++ java-lang/app/src/main/java/com/eomcs/quiz/ex01/Test22.java
@@ -1,8 +1,9 @@
 package com.eomcs.quiz.ex01;
+
 // copyright by codefights.com
-// 
+//
 // 숫자와 달의 영어 이름을 연결하라!
-// 단 숫자는 0 이상이다. 
+// 단 숫자는 0 이상이다.
 //
 // 1: "Jan"
 // 2: "Feb"
@@ -16,26 +17,23 @@ package com.eomcs.quiz.ex01;
 // 10: "Oct"
 // 11: "Nov"
 // 12: "Dec"
-// 예) 
-//    getMonthName(1) = "Jan"
-//    getMonthName(0) = "invalid month"
-// 
+// 예)
+// getMonthName(1) = "Jan"
+// getMonthName(0) = "invalid month"
+//
 //
 /*
-Map the given integer to a month.
-
-Example
-
-getMonthName(1) = "Jan"
-getMonthName(0) = "invalid month"
-
-[input] integer mo
-a non-negative integer
-
-[output] string
-
-a 3 letter abbreviation of month number mo or "invalid month" 
-if the month doesn't exist
+ * Map the given integer to a month.
+ * 
+ * Example
+ * 
+ * getMonthName(1) = "Jan" getMonthName(0) = "invalid month"
+ * 
+ * [input] integer mo a non-negative integer
+ * 
+ * [output] string
+ * 
+ * a 3 letter abbreviation of month number mo or "invalid month" if the month doesn't exist
  */
 //
 // [시간 복잡도]
@@ -46,11 +44,30 @@ public class Test22 {
   public static void main(String[] args) {
     System.out.println(getMonthName(1) == "Jan");
     System.out.println(getMonthName(12) == "Dec");
-    System.out.println(getMonthName(0) == "invalid month"); 
+    System.out.println(getMonthName(0) == "invalid month");
   }
 
   static String getMonthName(int mo) {
+    // 1: "Jan"
+    // 2: "Feb"
+    // 3: "Mar"
+    // 4: "Apr"
+    // 5: "May"
+    // 6: "Jun"
+    // 7: "Jul"
+    // 8: "Aug"
+    // 9: "Sep"
+    // 10: "Oct"
+    // 11: "Nov"
+    // 12: "Dec"
     // 이 메서드를 완성하시오!
-    return "";
+    mo += -1;
+    String[] month =
+        {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    if (mo < 0 || mo > month.length) {
+      return "invalid month";
+    } else {
+      return month[mo];
+    }
   }
 }
--- java-lang/app/src/main/java/com/eomcs/quiz/ex02/Test09.java
+++ java-lang/app/src/main/java/com/eomcs/quiz/ex02/Test09.java
@@ -1,29 +1,29 @@
 package com.eomcs.quiz.ex02;
 
 // copyright by codefights.com
-// 
+//
 // 주어진 배열을 꺼꾸로 나열하라!
-// 예) 
-//    myReverse([1, 3, 2]) = [2, 3, 1]
+// 예)
+// myReverse([1, 3, 2]) = [2, 3, 1]
 /*
-Reverse an array
-
-Example
-
-myReverse([1, 3, 2]) = [2, 3, 1]
-
-[input] array.integer input
-
-[output] array.integer
+ * Reverse an array
+ * 
+ * Example
+ * 
+ * myReverse([1, 3, 2]) = [2, 3, 1]
+ * 
+ * [input] array.integer input
+ * 
+ * [output] array.integer
  */
 //
 // [시간 복잡도]
-// - ? 
+// - ?
 //
 public class Test09 {
 
   public static void main(String[] args) {
-    int[] reverseList = myReverse(new int[]{9,8,7,6,5,4,3,2,1});
+    int[] reverseList = myReverse(new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1});
     for (int i : reverseList) {
       System.out.print(i + ",");
     }
@@ -31,6 +31,17 @@ public class Test09 {
 
   static int[] myReverse(int[] input) {
     // 이 메서드를 완성하시오!
+    //    int size = input.length;
+    //    int[] arr = new int[size];
+    //    for (int i = 0; i < size; i++) {
+    //      arr[i] = input[size - i - 1];
+    //    }
+    //    return arr;
+    for (int i = 0; i < input.length / 2; i++) {
+      int tmp = input[i];
+      input[i] = input[input.length - i - 1];
+      input[input.length - i - 1] = tmp;
+    }
     return input;
   }
 }
