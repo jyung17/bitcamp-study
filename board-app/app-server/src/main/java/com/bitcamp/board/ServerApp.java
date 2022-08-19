12d491a9c4477329fe9fb6fe07d8a9f81064b7a1
jyung17 <iotaxixi@gmail.com>
2022-08-19 16:52:10 +0900

    0819

--- board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/ServerApp.java
@@ -13,10 +13,7 @@ public class ServerApp {
   public static void main(String[] args) {
     System.out.println("[게시글 데이터 관리 서버]");
 
-    try (
-        // 네트워크 준비
-        // => 클라이언트 연결을 관리할 객체를 준비한다.
-        ServerSocket serverSocket = new ServerSocket(8888);) {
+    try (ServerSocket serverSocket = new ServerSocket(8888)) {
 
       System.out.println("서버 소켓 준비 완료!");
 
@@ -30,77 +27,29 @@ public class ServerApp {
       servletMap.put("member", new MemberServlet("member"));
 
       while (true) {
-        try (
-            // 클라이언트의 연결을 기다림
-            // => 클라이언트와 연결되면 그 클라이언트와 통신할 준비를 한다.
-            //    즉 Socket 객체 리턴
-            // => 클라이언트와 연결될 때까지 리턴하지 않는다.
-            Socket socket = serverSocket.accept();
-
-            // 클라이언트와 데이터를 주고 받는다.
-            // => 클라이언트가 보낸 데이터를 읽을 때 사용할 도구를 준비한다.
-            // => 데이터를 읽을 때 primitive type 또는 String 타입의 값을 
-            //    보다 손쉽게 읽을 수 있도록 기존의 입력 도구에 보조 도구(decorator)붙여 사용한다.
+        try (Socket socket = serverSocket.accept();
             DataInputStream in = new DataInputStream(socket.getInputStream());
-
-
-            // => 클라이언트로 데이터를 보낼 때 사용할 도구를 준비한다.
-            // => 데이터를 출력할 때 primitive type 또는 String 타입의 값을 
-            //    보다 손쉽게 출력 할 수 있도록 기존의 출력 도구에 보조 도구(decorator)붙여 사용한다.
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());) {
 
           System.out.println("클라이언트와 연결 되었음!");
 
-          // 클라이언트 요청을 처리할 객체 준비
-          //        BoardServlet boardServlet = new BoardServlet("board");
-          //        BoardServlet readingServlet = new BoardServlet("reading");
-          //        BoardServlet visitServlet = new BoardServlet("visit");
-          //        BoardServlet noticeServlet = new BoardServlet("notice");
-          //        BoardServlet dailyServlet = new BoardServlet("daily");
-          //        MemberServlet memberServlet = new MemberServlet("member");
+          // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
+          String dataName = in.readUTF();
 
-          while (true) {
-            // 클라이언트와 서버 사이에 정해진 규칙(protocol)에 따라 데이터를 주고 받는다.
-            String dataName = in.readUTF();
+          if (dataName.equals("exit")) {
+            break;
+          }
 
-            if (dataName.equals("exit")) {
-              break;
-            }
-
-            Servlet servlet = servletMap.get(dataName);
-            if (servlet != null) {
-              servlet.service(in, out);
-            } else {
-              out.writeUTF("fail");
-            }
-
-            //          switch (dataName) {
-            //            case "board":
-            //              boardServlet.service(in, out);
-            //              break;
-            //            case "reading":
-            //              readingServlet.service(in, out);
-            //              break;
-            //            case "visit":
-            //              visitServlet.service(in, out);
-            //              break;
-            //            case "notice":
-            //              noticeServlet.service(in, out);
-            //              break;
-            //            case "daily":
-            //              dailyServlet.service(in, out);
-            //              break;
-            //            case "member":
-            //              memberServlet.service(in, out);
-            //              break;
-            //            default:
-            //              out.writeUTF("fail");
-            //          }
+          Servlet servlet = servletMap.get(dataName);
+          if (servlet != null) {
+            servlet.service(in, out);
+          } else {
+            out.writeUTF("fail");
           }
 
           System.out.println("클라이언트와 연결을 끊었음!");
         } // 안쪽 try
-      } // while
+      }
     } catch (Exception e) {
       e.printStackTrace();
     } // 바깥 쪽 try 
