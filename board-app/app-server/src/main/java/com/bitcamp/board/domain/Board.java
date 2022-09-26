8f9119000590884aa8f2363fb9594d210ec18277
jyung17 <iotaxixi@gmail.com>
2022-09-26 17:21:57 +0900

    Feat: 063. OGNL 표기법으로 객체 프로퍼티를 좀 더 쉽게 다루기: EL 문법 사용

--- board-app/app-server/src/main/java/com/bitcamp/board/domain/Board.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/domain/Board.java
@@ -12,14 +12,70 @@ public class Board {
   public int viewCount;
   public Date createdDate;
 
+
+  // 프로퍼티(setter/getter) : no
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public int getMemberNo() {
+    return memberNo;
+  }
+
+  public void setMemberNo(int memberNo) {
+    this.memberNo = memberNo;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getViewCount() {
+    return viewCount;
+  }
+
+  public void setViewCount(int viewCount) {
+    this.viewCount = viewCount;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
   @Override
   public String toString() {
     return "Board [no=" + no + ", title=" + title + ", content=" + content + ", memberNo="
         + memberNo + ", password=" + password + ", viewCount=" + viewCount + ", createdDate="
         + createdDate + "]";
   }
-
-
 }
 
 
