d08b1c8521c7021282d0a35d970cd6958e52066f
jyung17 <iotaxixi@gmail.com>
2022-10-04 15:43:35 +0900

    Feat: 070. 파일을 업로드하기 II: Servlet API로 파일업로드 처리하기 - Servlet에서 제공하는 API활용

--- board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardAddController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardAddController.java
@@ -1,23 +1,25 @@
 package com.bitcamp.board.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import javax.servlet.http.Part;
 import com.bitcamp.board.dao.BoardDao;
 import com.bitcamp.board.domain.AttachedFile;
 import com.bitcamp.board.domain.Board;
 import com.bitcamp.board.domain.Member;
 
+// Servlet API에서 제공하는 Multipart/form-data 처리기를 사용하려면
+// 서블릿에 다음 애노테이션으로 설정해야한다.
+@MultipartConfig(maxFileSize = 1024 * 1024 * 10) // 최대 10M까지 업로드 혀용
 @WebServlet("/board/add")
 public class BoardAddController extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -34,84 +36,47 @@ public class BoardAddController extends HttpServlet {
       throws ServletException, IOException {
 
     try {
-      // URL 디코딩 한 바이트를 UTF-16으로 변환하기 전에
-      // 그 바이트의 characterset이 무엇인지 알려줘야 한다.
-      // 안 알려주면 그 디코딩 바이트가 ASCII 코드라고 간주한다.
-      // UTF-8 코드를 ASCII 코드라고 잘못 판단하니까 UTF-16으로 바꿀 때 오류가 발생하는 것이다.
-      // 물론 영어나 숫자는 ASCII 코드와 UTF-8이 같기 때문에 UTF-16으로 변환하더라도 문제가 되지 않는다.
-      // 그러나 한글은 UTF-8 코드의 3바이트를 묶어서 UTF-16 2바이트로 변환해야 하는데,
-      // 영어라고 간주하고 각각의 1바이트를 2바이트로 변환하니까 문제가 발생하는 것이다.
+      request.setCharacterEncoding("UTF-8");
+      Board board = new Board();
+      board.setTitle(request.getParameter("title"));
+      board.setContent(request.getParameter("content"));
+      System.out.println(board);
 
-      // getParameter()를 최초로 호출하기 전에 먼저 실행
-      // 단 한번이라도 호출한 후에 실행하면 무효하다.
-      //       request.setCharacterEncoding("UTF-8");
-
-      //multiparg/form-data 형식으로 요청한 데이터에 대해서는
-      // 다음과 같이 요청 데이터의 인코딩을 지정할 수 없다.
-      //       request.setCharacterEncoding("UTF-8");
-
-      // 멀티파트 데이터 처리하기
-      // 1) 클라이언트가 보낸 멀티파트 데이터를 임시 보관할 객체 준비
-      //  - 임시 폴더에 저장한다.
-      DiskFileItemFactory factory = new DiskFileItemFactory(); // FileSystem(HDD(SSD)) 임시 폴더에 저장한다.
-
-      // 2) 멀티파트 데이터를 분석할 객체 준비
-      //  - 멀티파트 데이터를 파트 별로 쪼개서 이름과 데이터를 분리하여
-      // DiskFileItemFactory를 이용하여 임시 폴더에 저장한다.
-      ServletFileUpload upload = new ServletFileUpload(factory);
-
-      // 3) HttpServletRequest 객체를 통해 데이터를 읽어서 멀티파트 데이터를 처리한다.
-      // - 각각의 파트 정보는 FileItem 객체에 담긴다.
-      // - 그 FileItem 객체들을 List에 담아 리턴한다.
-      // - 물론 업로드 된 파일의 데이터는 DiskFileItemFactory에서 관리하고 있다.
-      List<FileItem> items = upload.parseRequest(request);
-
-      // 클라이언트가 멀티파트로 보낸 데이터를 저장할 도메인 객체를 준비한다.
-      Board board = new Board(); //게시글 데이터를 저장함
 
       // 첨부파일명을 저장할 컬렉션 객체 준비
       List<AttachedFile> attachedFiles = new ArrayList<>();
 
       // 임시 폴더에 저장된 파일을 옮길 폴더 경로 알아내기
       String dirPath = request.getServletContext().getRealPath("/board/files");
-      //System.out.println(dirPath + "/" + filename);
 
-      // 각 파트의 데이터를 꺼내 Board 객체에 담는다.
-      for (FileItem item : items) {
-        if (item.isFormField()) { // 일반 입력 값이라
-          String paramName = item.getFieldName(); // 
-          String paramValue = item.getString("UTF-8"); // 데이터를 UTF-8 디코딩해 paramValue에 저장 
-          //System.out.println("getFieldName : getString = " + paramName + " : " + paramValue);
+      Collection<Part> parts = request.getParts();
 
-          switch (paramName) {
-            case "title":
-              board.setTitle(paramValue);
-            case "content":
-              board.setContent(paramValue);
-          }
+      // for (Part part : parts){
+      //    System.out.println(part.getName());
+      //}
+      //      Board [no=0, title=ABC가각간, content=ABC가각간, password=null, viewCount=0, createdDate=null, writer=null]
+      //          title
+      //          content
+      //          files
+      //          files
+      //          files
 
-        } else { // 파일이라면
-          // 절대 충돌이 일어나지않는, 값이 같을수 없
-          // 다른 클라이언트가 보낸 파일명과 중복되지 않도록 임의의 새 파일명을 생성한다.
-          String filename = UUID.randomUUID().toString();
-
-          // 파일이름을 AttachedFile 객체에 담은 후
-          // DB에 저장할 수 있도록 컬렉션에 저장한다.
-          //          AttachedFile attachedFile = new AttachedFile(filename);
-          //          attachedFiles.add(attachedFile);
-          attachedFiles.add(new AttachedFile(filename));
-
-          // FileItem 객체가 가리키는 임시폴더에 저장된 파일을 
-          // 우리가 지정한 디렉토리로 옮긴다.
-          // 이때 파일명은 원래의 이름 대신 우리가 UUID로 생성한 이름을 사용한다.
-          item.write(new File(dirPath + "/" + filename));
+      for (Part part : parts) {
+        if (!part.getName().equals("files")) {
+          continue;
         }
+
+        String filename = UUID.randomUUID().toString();
+        // 우리가 지정한 디렉토리로 옮긴다.
+        // 이때 파일명은 원래의 이름 대신 우리가 UUID로 생성한 이름을 사용한다.
+        part.write(dirPath + "/" + filename);
+        attachedFiles.add(new AttachedFile(filename));
       }
 
-      // Board 객체에 파일명 목록을 담고 있는 컬랙션 객체를 저장한다.
+      // Board 객체에서 파일명 목록을 담고 있는 컬렉션 객체를 저장한다.
       board.setAttachedFiles(attachedFiles);
 
-      // Board 객체에 사용자 정보를 저장한다.
+      // Board 객체에 로그인 사용자 정보를 저장한다.
       Member loginMember = (Member) request.getSession().getAttribute("loginMember");
       board.setWriter(loginMember);
 
@@ -120,8 +85,9 @@ public class BoardAddController extends HttpServlet {
       }
 
       response.sendRedirect("list");
+    } catch (
 
-    } catch (Exception e) {
+    Exception e) {
       request.setAttribute("exception", e);
       request.getRequestDispatcher("/error.jsp").forward(request, response);
     }
--- board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardUpdateController.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/controller/BoardUpdateController.java
@@ -1,24 +1,23 @@
 package com.bitcamp.board.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import javax.servlet.http.Part;
 import com.bitcamp.board.dao.BoardDao;
 import com.bitcamp.board.domain.AttachedFile;
 import com.bitcamp.board.domain.Board;
 import com.bitcamp.board.domain.Member;
 
-
+@MultipartConfig(maxFileSize = 1024 * 1024 * 10)
 @WebServlet("/board/update")
 public class BoardUpdateController extends HttpServlet {
   private static final long serialVersionUID = 1L;
@@ -35,45 +34,27 @@ public class BoardUpdateController extends HttpServlet {
       throws ServletException, IOException {
 
     try {
-      DiskFileItemFactory factory = new DiskFileItemFactory();
-      ServletFileUpload upload = new ServletFileUpload(factory);
-      List<FileItem> items = upload.parseRequest(request);
-
+      request.setCharacterEncoding("UTF-8");
       Board board = new Board();
+      board.setNo(Integer.parseInt(request.getParameter("no")));
+      board.setTitle(request.getParameter("title"));
+      board.setContent(request.getParameter("content"));
+
       List<AttachedFile> attachedFiles = new ArrayList<>();
+      String dirPath = request.getServletContext().getRealPath("/board/files");
+      Collection<Part> parts = request.getParts();
 
-      // 첨부파일을 저장할 OS의 파일시스템 경로를 알아낸다.
-      String dirPath = this.getServletContext().getRealPath("/board/files");
+      for (Part part : parts) {
+        if (!part.getName().equals("files"))
+          continue;
 
-      for (FileItem item : items) {
-        if (item.isFormField()) {
-          String paramName = item.getFieldName();
-          String paramValue = item.getString("UTF-8");
-
-          switch (paramName) {
-            case "no":
-              board.setNo(Integer.parseInt(paramValue));
-            case "title":
-              board.setTitle(paramValue);
-            case "content":
-              board.setContent(paramValue);
-          }
-
-        } else {
-          //첨부파일을 저장할 때 사용할 파일명을 생성한다.
-          String filename = UUID.randomUUID().toString();
-
-          // 지정한 위치에 생성한 이름을 첨부파일을 저장한다.
-          item.write(new File(dirPath + "/" + filename));
-
-          // 첨부파일의 이름을 DB에 저장할 수있도록 List에 보관한다.
-          attachedFiles.add(new AttachedFile(filename));
-        }
+        String filename = UUID.randomUUID().toString();
+        part.write(dirPath + "/" + filename);
+        attachedFiles.add(new AttachedFile(filename));
       }
 
       board.setAttachedFiles(attachedFiles);
 
-      // 게시글 작성자인지 검사한다.
       Member loginMember = (Member) request.getSession().getAttribute("loginMember");
       if (boardDao.findByNo(board.getNo()).getWriter().getNo() != loginMember.getNo()) {
         throw new Exception("게시글 작성자가 아닙니다.");
