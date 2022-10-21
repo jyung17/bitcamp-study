e0a82443615ed0a1f33fa194b129dade1afc558a
jyung17 <iotaxixi@gmail.com>
2022-10-21 17:05:38 +0900

    .

--- board-app/app-server/src/main/java/com/bitcamp/board/App.java
+++ board-app/app-server/src/main/java/com/bitcamp/board/App.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 // @Transactional 애노테이션을 사용하여
 // 트랜잭션을 제어할 수 있게 기능을 활성화 시킨다.
-@Controller
 @EnableTransactionManagement
 @SpringBootApplication
 public class App {
